6dbca8872dcdad8cbfee0876c2ebb5a348fa45e8
lili205an241 <lilian_carmo05@hotmail.com>
2024-10-24 11:11:18 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,53 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+
+criaCartao(
+    'História',
+    'Quem foi o primeiro imperador do Brasil?',
+    'O primeiro imperador do Brasil foi Dom Pedro I'
+)
+
+criaCartao(
+    'História',
+    'Qual foi o principal motivo da Revolução Francesa?',
+    'Desigualdade social e econõmica entre os três estados, altos impostos e a insatisfação com a monarquia'
+)
+
+criaCartao(
+    'Biologia',
+    'O que são células procarióticas?',
+    'São células celulares que não possuem núcleo organizado nem organelas membranosas, como as bactérias'
+)
+
+criaCartao(
+    'Biologia',
+    'Qual é o principal órgão do sistema nervoso central?',
+    'O cérebro'
+)
+
+criaCartao(
+    'Filosofia',
+    'O que é ética?',
+    'Ética é o estudo dos princípios que orientam o comportamento humano, distinguindo o certo do errado'
+)
